fce0130b10186f09252c20ca37a3ff1f0ea8e0cc
michaaaaaal <007michalsliwinski@gmail.com>
2025-06-17 16:21:56 +0200

    task3

--- code/Task3.py
+++ code/Task3.py
@@ -8,7 +8,14 @@ np.random.seed(60) #reproducability
 mu = 115
 sigma = 10
 n = 50
+tau = 0.9
 
 demand_sample = np.random.normal(loc=mu, scale=sigma, size=n)
 
+mu_hat = np.mean(demand_sample)
+sigma_hat = np.std(demand_sample, ddof=1)
+
+Q_star =lambda tau: norm.ppf(tau,loc=115,scale=10)
+
 print(demand_sample)
+print("True optimal Q:", Q_star(tau))
