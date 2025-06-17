ab2f08732d24ba753ca96a1bc23a40bdf7ec7d08
michaaaaaal <007michalsliwinski@gmail.com>
2025-06-17 22:22:39 +0200

    changed task3 tables

--- code/Task3_tables.py
+++ code/Task3_tables.py
@@ -2,13 +2,15 @@ import pandas as pd
 
 df = pd.read_csv("./report/figures/task3/task3_simulation_results.csv")
 
-rmse_pivot_p      = df.pivot(index="tau", columns="n", values="RMSE (Parametric)")
-rmse_pivot_np     = df.pivot(index="tau", columns="n", values="RMSE (Nonparametric)")
-rmse_pivot_ratio  = df.pivot(index="tau", columns="n", values="RMSE ratio (Nonparametric / Parametric)")
-plr_pivot_np      = df.pivot(index="tau", columns="n", values="PLR (Nonparametric)")
-plr_pivot_ratio   = df.pivot(index="tau", columns="n", values="PLR ratio (Nonparametric / Parametric)")
+rmse_pivot_p = df.pivot(index="n", columns="tau", values="RMSE (Parametric)")
+rmse_pivot_np = df.pivot(index="n", columns="tau", values="RMSE (Nonparametric)")
+rmse_pivot_ratio = df.pivot(index="n", columns="tau", values="RMSE ratio (Nonparametric / Parametric)")
+plr_pivot_np = df.pivot(index="n", columns="tau", values="PLR (Nonparametric)")
+plr_pivot_ratio = df.pivot(index="n", columns="tau", values="PLR ratio (Nonparametric / Parametric)")
 
-with pd.ExcelWriter("./report/figures/task3/task3_tables.xlsx") as writer:
+
+output_path = "./report/figures/task3/task3_tables.xlsx"
+with pd.ExcelWriter(output_path) as writer:
     rmse_pivot_p.to_excel(writer, sheet_name="RMSE Parametric")
     rmse_pivot_np.to_excel(writer, sheet_name="RMSE Nonparametric")
     rmse_pivot_ratio.to_excel(writer, sheet_name="RMSE Ratio")
