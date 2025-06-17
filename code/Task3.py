b01dd432d252f98b99c95bbb388914b0eb46d60e
michaaaaaal <007michalsliwinski@gmail.com>
2025-06-17 16:30:53 +0200

    task3

--- code/Task3.py
+++ code/Task3.py
@@ -11,11 +11,14 @@ n = 50
 tau = 0.9
 
 demand_sample = np.random.normal(loc=mu, scale=sigma, size=n)
-
 mu_hat = np.mean(demand_sample)
 sigma_hat = np.std(demand_sample, ddof=1)
-
 Q_star =lambda tau: norm.ppf(tau,loc=115,scale=10)
 
-print(demand_sample)
+#non-parametric
+sorted_sample = np.sort(demand_sample)
+index = int(np.ceil(tau * n)) - 1
+Q_np = sorted_sample[index]
+
+print(Q_np)
 print("True optimal Q:", Q_star(tau))
